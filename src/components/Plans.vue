d50d06406c79d54a1ad30bc61e00e5ecaed09cdd
classyrazy <classyrazy014@gmail.com>
2023-08-06 20:16:17 +0100

    Fix: Api was not working

--- src/components/Plans.vue
+++ src/components/Plans.vue
@@ -69,11 +69,41 @@ import { onMounted, ref } from 'vue';
 import useAxios from '../composables/useAxios';
 const plans = ref<{
     id: number, name: string, amount: string, description: string, cta: string, call_reception: string, calls_per_week: string, agent: string
-}[]>([])
+}[]>([
+    {
+        id: 1,
+        name: "Starter Plan",
+        amount: "25000",
+        description: "For businesses with few customers",
+        cta: "Subscribe Here",
+        call_reception: "Receive calls only",
+        calls_per_week: "Less than 30 calls per week",
+        agent: "Shared agents"
+    },
+    {
+        id: 2,
+        name: "Growth Plan",
+        amount: "50000",
+        description: "For growing businesses",
+        cta: "Subscribe Here",
+        call_reception: "Receive and make calls",
+        calls_per_week: "30 - 70 calls per week",
+        agent: "Semi-dedicated agents"
+    },
+    {
+        id: 3,
+        name: "All Access",
+        amount: "Custom",
+        description: "For e-commerce and retail businesses",
+        cta: "Calculate Your Cost",
+        call_reception: "Receive and make calls",
+        calls_per_week: "You choose",
+        agent: "Fully dedicated agents"
+    }])
 const handleFetchPlans = async () => {
     try {
         const req = useAxios().get("/swQ1ic")
-        const res :any = await req
+        const res: any = await req
         if (res.data) {
             plans.value = res.data
         }
@@ -83,7 +113,8 @@ const handleFetchPlans = async () => {
 
 }
 onMounted(() => {
-    handleFetchPlans()
+    // API was not working
+    // handleFetchPlans()
 })
 
 </script>
