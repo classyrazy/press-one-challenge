f560cc0d3d23a5360a29472cc7306ad07b312515
classyrazy <classyrazy014@gmail.com>
2023-08-06 12:48:31 +0100

    Minor refactor

--- src/components/Plans.vue
+++ src/components/Plans.vue
@@ -17,22 +17,46 @@
             </div>
             <div class="plan-details">
                 <ul class="detail detail-default">
-                    <li><p>Call Reception</p></li>
-                    <li><p>Receive calls only</p></li>
-                    <li><p>Receive and make calls</p></li>
-                    <li><p class="end">Receive and make calls</p></li>
+                    <li>
+                        <p>Call Reception</p>
+                    </li>
+                    <li>
+                        <p>Receive calls only</p>
+                    </li>
+                    <li>
+                        <p>Receive and make calls</p>
+                    </li>
+                    <li>
+                        <p class="end">Receive and make calls</p>
+                    </li>
                 </ul>
                 <ul class="detail detail-unique">
-                    <li><p>Calls per Week</p></li>
-                    <li><p>Receive calls only</p></li>
-                    <li><p>Receive and make calls</p></li>
-                    <li><p class="end">You choose</p></li>
+                    <li>
+                        <p>Calls per Week</p>
+                    </li>
+                    <li>
+                        <p>Receive calls only</p>
+                    </li>
+                    <li>
+                        <p>Receive and make calls</p>
+                    </li>
+                    <li>
+                        <p class="end">You choose</p>
+                    </li>
                 </ul>
                 <ul class="detail detail-default">
-                    <li><p>Agents</p></li>
-                    <li><p>Shared agents</p></li>
-                    <li><p>Semi-dedicated agents</p></li>
-                    <li><p class="end">Fully dedicated agents</p></li>
+                    <li>
+                        <p>Agents</p>
+                    </li>
+                    <li>
+                        <p>Shared agents</p>
+                    </li>
+                    <li>
+                        <p>Semi-dedicated agents</p>
+                    </li>
+                    <li>
+                        <p class="end">Fully dedicated agents</p>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -43,17 +67,19 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 import useAxios from '../composables/useAxios';
-const plans = ref([])
-const handleFetchPlans = async() => {
+const plans = ref<{
+    id: number, name: string, amount: string, description: string, cta: string, call_reception: string, calls_per_week: string, agent: string
+}[]>([])
+const handleFetchPlans = async () => {
     try {
-            const req = useAxios().get("/swQ1ic")
-            const res = await req
-            if (res.data) {
-               plans.value = res.data
-            }
-        } catch (err: any) {
-            console.log(err) 
+        const req = useAxios().get("/swQ1ic")
+        const res = await req
+        if (res.data) {
+            plans.value = res.data
         }
+    } catch (err: any) {
+        console.log(err)
+    }
 
 }
 onMounted(() => {
